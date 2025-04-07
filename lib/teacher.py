dcb540b50e6a0cfeb78b7a24b9cf348cccfe38e2
DAVIDMWELU <davidmwelu@gmail.com>
2025-04-07 15:02:05 +0300

    inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,15 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [  # ✅ Initialize with some info
+            "Math basics",
+            "History of Africa",
+            "Python programming",
+            "Photosynthesis",
+            "The Solar System"
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
